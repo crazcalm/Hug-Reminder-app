bab4fac4a5b17d63fb1b0d6dc8fa7add479f0ca8
Marcus Willock <crazcalm@gmail.com>
2022-06-14 16:33:14 -0400

    Getting some of the basics setup -- dotenv, clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::builder::Arg;
+use clap::Command;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let cli_app = Command::new("Hug-Reminder")
+        .about("About section")
+        .args(&[Arg::new("env-file")
+            .short('e')
+            .long("env_file")
+            .default_value("dev.env")
+            .help("Path to env file")]);
+
+    let matches = cli_app.get_matches();
+
+    let env_file_path = matches.get_one::<String>("env-file").unwrap();
+    println!("{:?}", env_file_path);
+
+    println!("matches: {:?}", matches);
+
+    let _ = dotenv::from_filename(env_file_path).ok();
+
+    let email =
+        env::var("YAHOO_EMAIL").expect("Unable to find 'YAHOO_EMAIL' in the environment variables");
+
+    println!("email: {:?}", email);
 }
