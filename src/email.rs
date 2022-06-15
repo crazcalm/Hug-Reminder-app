c47513988cafb150e62445b08750a93104b68bdf
Marcus Willock <crazcalm@gmail.com>
2022-06-15 12:26:22 -0400

    Add a test to email file

--- src/email.rs
+++ src/email.rs
@@ -63,3 +63,79 @@ pub fn send(
         Err(e) => Err(format!("Could not send email: {:?}", e)),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::env;
+
+    use super::*;
+
+    fn setup_environment_variables() -> HashMap<&'static str, &'static str> {
+        let map_values = HashMap::from([
+            ("YAHOO_EMAIL", "fake_email"),
+            ("YAHOO_PASSWORD", "fake_password"),
+            ("PHONE_NUMBER_EMAIL", "fake_phone_number_email"),
+        ]);
+
+        for (key, value) in &map_values {
+            env::set_var(key, value);
+        }
+
+        map_values
+    }
+
+    fn tear_down_environment_variables() {
+        let keys = vec!["YAHOO_EMAIL", "YAHOO_PASSWORD", "PHONE_NUMBER_EMAIL"];
+
+        for key in &keys {
+            env::remove_var(key);
+        }
+    }
+
+    /*
+     * The test run in parallel, as such, setup from the other test causes this one to fail.
+     *
+    #[test]
+    fn test_get_info_errors() {
+        tear_down_environment_variables();
+
+        let expected = "Unable to find YAHOO_EMAIL in environment variables, Unable to find YAHOO_PASSWORD in environment variables, Unable to find PHONE_NUMBER_EMAIL in environment variables".to_string();
+        let result = get_info();
+
+        assert!(result.is_err());
+
+        assert_eq!(result.err().unwrap(), expected);
+    }
+    */
+
+    #[test]
+    fn test_get_info() {
+        let map_values = setup_environment_variables();
+
+        let (email, password, to_email) = get_info().unwrap();
+
+        assert_eq!(
+            email,
+            map_values.get("YAHOO_EMAIL").unwrap().clone().to_string()
+        );
+        assert_eq!(
+            password,
+            map_values
+                .get("YAHOO_PASSWORD")
+                .unwrap()
+                .clone()
+                .to_string()
+        );
+        assert_eq!(
+            to_email,
+            map_values
+                .get("PHONE_NUMBER_EMAIL")
+                .unwrap()
+                .clone()
+                .to_string()
+        );
+
+        tear_down_environment_variables();
+    }
+}
